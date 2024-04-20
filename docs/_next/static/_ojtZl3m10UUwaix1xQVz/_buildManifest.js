caf1cf7aae185d44b3c5e857f1a2f9e30b27721a
Erik Brinkman <erik.brinkman@gmail.com>
2024-04-20 14:10:08 -0400

    new release

--- docs/_next/static/_ojtZl3m10UUwaix1xQVz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-f1897d4cf92d05ef.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
